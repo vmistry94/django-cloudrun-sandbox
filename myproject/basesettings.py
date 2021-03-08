9399726ba1a1f912f38318811c70577bf5d7df95
Viral <vmistry94@gmail.com>
2021-03-08 23:41:14 +0000

    continous deployment test #3

--- myproject/basesettings.py
+++ myproject/basesettings.py
@@ -19,15 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+wx1s%kvdsszl$s1_x(6aex31434)_inosgdpgjb3p(-_rmm+&'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
